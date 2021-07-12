a31bcc07727894e5ca482af9cda8320215e9e0d1
bae0053 <bae8719@naver.com>
2021-07-12 14:13:59 +0900

    path modified

--- TM.js
+++ TM.js
@@ -4,7 +4,7 @@
 // the link to your model provided by Teachable Machine export panel
 // 모델이 업로드돼있는 URL
 //const URL = "https://teachablemachine.withgoogle.com/models/uihKvlGFe/";
-const URL = "./tm-my-image-model/";
+const URL = "/tree/master/tm-my-image-model/";
 import { getAudioUrl } from './node_modules/google-tts-api';
 
 let model, webcam, labelContainer, maxPredictions;
